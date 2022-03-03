b254620e274f0dda6b2b6c9409eaf3e76b4c6e6c
dmrickey <dmrickey@gmail.com>
2022-03-03 15:07:54 -0600

    More cone-placement menu work

--- src/module/scripts/template-placement/cones/cone-placement.js
+++ src/module/scripts/template-placement/cones/cone-placement.js
@@ -1,4 +1,4 @@
-import { MODULE_NAME } from '../../../consts';
+import { CONSTS, MODULE_NAME } from '../../../consts';
 import * as create from './placements';
 
 export class ConePlacement {
@@ -8,66 +8,46 @@ export class ConePlacement {
         this.itemPf = itemPf;
     }
 
-    placementTypes = {
-        use15: {
-            key: 'use15',
-            label: `Use Standard 15' Cone`,
-            func: async (options, token) => await create.createCone15(options, token),
+    _placementTypes = {
+        [CONSTS.placement.cone.self]: {
+            key: CONSTS.placement.cone.self,
+            label: 'Originate from Caster',
         },
-        useDefault: {
-            key: 'useDefault',
+        [CONSTS.placement.cone.selectTargetSquare]: {
+            key: CONSTS.placement.cone.self,
+            label: 'Originate from Selection',
+        },
+        [CONSTS.placement.cone.alt15]: {
+            key: CONSTS.placement.cone.self,
+            label: `Alternate 15' Cone`,
+        },
+        [CONSTS.placement.useDefault]: {
+            key: CONSTS.placement.useDefault,
             label: 'Use System Default',
-            func: async (_options, _token, wrapped) => {
-                const defaultTemplateResult = await wrapped();
-                if (defaultTemplateResult.result) {
-                    return defaultTemplateResult;
-                }
-            }
-        },
-        useAlternate: {
-            key: 'useAlternate',
-            label: `Use Alternate 15' Cone`,
-            func: async (options, token) => await create.createConeOtherDistance(options, token),
-        },
-        selectSource: {
-            key: 'selectSource',
-            label: 'Select Source Square',
-            func: async (options) => await create.createCone(options),
-        },
-
-        // if no flag is set
-        [undefined]: {
-            func: async (options, token) => await create.createCone(options, token),
         },
     };
-
-    async createCurrentTemplate(options, token, wrapped) {
-        const getHandler = (key) => this.placementTypes[key] || this.placementTypes[undefined];
-        const handler = getHandler(this.itemPf.getFlag(MODULE_NAME, ConePlacement.placementKey));
-        return await handler.func(options, token, wrapped);
-    }
 
     /**
      * Shows the menu, saves the result, then returns the result
      *
-     * @param {*} is15Feet If the cone is 15 or not
-     *
      * @returns {bool} True if a placement type was selected, false if it was canceled.
      */
-    async showPlacementMenu(is15Feet) {
+    async showPlacementMenu() {
+        const is15Feet = this._getSize() === 15;
+        const showAlternateOption = is15Feet; // && read from settings to get if alternative is allowed
+
         const makeButton = ({ key: value, label }) => ({ label, value });
 
-        const buttons = [
-            makeButton(this.placementTypes.selectSource),
-            makeButton(this.placementTypes.useDefault),
-            {
-                label: 'Cancel'
-            },
-        ];
-        if (is15Feet) {
-            buttons.unshift(makeButton(this.placementTypes.useAlternate));
-            buttons.unshift(makeButton(this.placementTypes.use15));
+        const buttons = [];
+        buttons.push(makeButton(this._placementTypes[CONSTS.placement.cone.self]));
+        if (showAlternateOption) {
+            buttons.push(makeButton(this.placementTypes[CONSTS.placement.cone.alt15]));
         }
+        buttons.push(makeButton(this._placementTypes[CONSTS.placement.cone.selectTargetSquare]));
+        buttons.push(makeButton(this._placementTypes[CONSTS.placement.useSystem]));
+        buttons.push({
+            label: 'Cancel'
+        });
 
         const dialogResult = await warpgate.menu({
             inputs: [{ type: 'info', label: `Select placement type for this ${this.itemPf.type}` }],
@@ -83,5 +63,12 @@ export class ConePlacement {
         }
 
         return !!placementType;
+    }
+    
+    _getSize() {
+        const rollData = null; // todo get this value from itemPF
+        return typeof this.itemPf.data.data.measureTemplate.size === 'string'
+            ? RollPF.safeTotal(this.itemPf.data.data.measureTemplate.size, rollData)
+            : game.pf1.utils.convertDistance(this.itemPf.data.data.measureTemplate.size)[0];
     }
 }
