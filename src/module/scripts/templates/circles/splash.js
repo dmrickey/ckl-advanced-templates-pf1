5ecba498913dce0bb2bb6ba8d4d0c098461f1007
dmrickey <dmrickey@gmail.com>
2025-07-28 09:51:03 -0500

    Simplified splash logic

--- src/module/scripts/templates/circles/splash.js
+++ src/module/scripts/templates/circles/splash.js
@@ -3,12 +3,8 @@ import { AbilityTemplateAdvanced } from '../ability-template';
 export class CircleSplash extends AbilityTemplateAdvanced {
     /** @override */
     get _snapMode() {
-        const boundsContains = (bounds, point) =>
-            bounds.left <= point.x
-            && point.x <= bounds.right
-            && bounds.top <= point.y
-            && point.y <= bounds.bottom;
-        const found = !!canvas.tokens.placeables.map(x => x.bounds).find(b => boundsContains(b, canvas.mousePosition));
+        const { x, y } = canvas.mousePosition;
+        const found = !!canvas.tokens.placeables.map(x => x.bounds).find(b => b.contains(x, y));
         return found ? CONST.GRID_SNAPPING_MODES.CENTER : CONST.GRID_SNAPPING_MODES.VERTEX;
     }
 }
