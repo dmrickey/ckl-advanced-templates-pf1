3b1134ea006ca6ed69572b91eedf068c825e7a23
dmrickey <dmrickey@gmail.com>
2025-08-12 07:33:49 -0500

    Fixed splah on non-square grids

--- src/module/scripts/templates/circles/splash.js
+++ src/module/scripts/templates/circles/splash.js
@@ -7,4 +7,24 @@ export class CircleSplash extends AbilityTemplateAdvanced {
         const found = !!canvas.tokens.placeables.map(x => x.bounds).find(b => b.contains(x, y));
         return found ? CONST.GRID_SNAPPING_MODES.CENTER : CONST.GRID_SNAPPING_MODES.VERTEX;
     }
+
+    #originalDistance = undefined;
+    /** */
+    async _onMove(event) {
+        if (!canvas.scene.grid.isSquare && !this._isDrag) {
+            if (this.#originalDistance === undefined) {
+                this.#originalDistance = this.document.distance;
+            }
+
+            const { x, y } = canvas.mousePosition;
+            const found = !!canvas.tokens.placeables.map(x => x.bounds).find(b => b.contains(x, y));
+            if (found) {
+                this.document.distance = this.#originalDistance + 5;
+            }
+            else {
+                this.document.distance = this.#originalDistance;
+            }
+        }
+        await super._onMove(event);
+    }
 }
